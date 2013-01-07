7f5f8dd3c67e4ee138b1f8807c07ffd7c5cababb
Bryan Hansen <bryan@seatme.com>
2013-01-07 11:29:16 -0800

    Now enabling accurate tracking when close to event time.

--- Herd/HRDAppDelegate.m
+++ Herd/HRDAppDelegate.m
@@ -11,11 +11,19 @@
 #import "HRDAPI.h"
 #import "NSUserDefaults+Herd.h"
 
-static CGFloat const EventSecondsProximity = 30.0f * 66.0f;
+static CGFloat const EventSecondsProximity = 30.0f * 60.0f;
+
+typedef enum {
+    EventTimeProximityBefore = 0,
+    EventTimeProximitySoonBefore,
+    EventTimeProximitySoonAfter,
+    EventTimeProximityAfter
+} EventTimeProximity;
 
 @interface HRDAppDelegate () {
     CLLocationManager *_locationManager;
     UIBackgroundTaskIdentifier _backgroundLocationTask;
+    BOOL _isTrackingLocationAccurately;
 }
 
 @end
@@ -41,6 +49,8 @@ static CGFloat const EventSecondsProximity = 30.0f * 66.0f;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    _isTrackingLocationAccurately = NO;
+
     if ([NSUserDefaults standardUserDefaults].trackingUser) {
         [self updateTrackingStatus];
     }
@@ -115,36 +125,37 @@ static CGFloat const EventSecondsProximity = 30.0f * 66.0f;
 
 - (void)updateTrackingStatus
 {
-    NSDate *eventDate = [NSUserDefaults standardUserDefaults].eventDate;
+    EventTimeProximity eventProximity = [self eventTimeProximity];
     
-    NSTimeInterval secondsSinceEvent = [[NSDate date] timeIntervalSinceDate:eventDate];
-    
-    if (!eventDate || secondsSinceEvent > EventSecondsProximity) {
+    if (eventProximity == EventTimeProximityAfter) {
         [NSUserDefaults standardUserDefaults].trackingUser = NO;
     }
     
     if (![NSUserDefaults standardUserDefaults].trackingUser) {
         [self.locationManager stopUpdatingLocation];
+        _isTrackingLocationAccurately = NO;
         return;
     }
     
     BOOL isInBackground = [UIApplication sharedApplication].applicationState == UIApplicationStateBackground;
-        
-    BOOL isCloseToEvent = fabs(secondsSinceEvent) < EventSecondsProximity;
     
-    if (isCloseToEvent || !isInBackground) {
+    if (([self eventTimeProximity] == EventTimeProximitySoonBefore || [self eventTimeProximity] == EventTimeProximitySoonAfter) || !isInBackground) {
         [self.locationManager stopUpdatingLocation];
         [self.locationManager startUpdatingLocation];
+        _isTrackingLocationAccurately = YES;
         
     } else {
         [self.locationManager stopUpdatingLocation];
         [self.locationManager startMonitoringSignificantLocationChanges];
+        _isTrackingLocationAccurately = NO;
     }
 }
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
 {
     BOOL isInBackground = [UIApplication sharedApplication].applicationState == UIApplicationStateBackground;
+    
+    if (isInBackground )
     
     if (locations.count) {
         [self sendLocationUpdate:locations[locations.count - 1] isInBackground:isInBackground];
@@ -160,6 +171,10 @@ static CGFloat const EventSecondsProximity = 30.0f * 66.0f;
             [[UIApplication sharedApplication] endBackgroundTask:_backgroundLocationTask];
         }];
         
+        if (!_isTrackingLocationAccurately && ([self eventTimeProximity] == EventTimeProximitySoonBefore || [self eventTimeProximity] == EventTimeProximitySoonAfter)) {
+            [self updateTrackingStatus];
+        }
+        
         [HRDAPI updateUserWithLocation:location.coordinate];
         
         if (_backgroundLocationTask != UIBackgroundTaskInvalid) {
@@ -171,6 +186,27 @@ static CGFloat const EventSecondsProximity = 30.0f * 66.0f;
         NSLog(@"Sending location update in foreground: %@", location);
         
         [HRDAPI updateUserWithLocation:location.coordinate];
+    }
+}
+
+
+#pragma mark - Private
+
+- (EventTimeProximity)eventTimeProximity
+{
+    NSDate *date = [NSUserDefaults standardUserDefaults].eventDate;
+    
+    if (!date) return EventTimeProximityAfter;
+    
+    NSTimeInterval secondsSinceEvent = [[NSDate date] timeIntervalSinceDate:date];
+    
+    BOOL isCloseToEventTime = fabs(secondsSinceEvent) < EventSecondsProximity;
+
+    if (secondsSinceEvent <= 0) {
+        return isCloseToEventTime ? EventTimeProximitySoonBefore : EventTimeProximityBefore;
+    
+    } else {
+        return isCloseToEventTime ? EventTimeProximitySoonAfter : EventTimeProximityAfter;
     }
 }
 
